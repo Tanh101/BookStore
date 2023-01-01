63f2a9dda61ba97cdf9de9bb8714a40798aafb2e
Van Tanh Ly <lyvantanh101@gmail.com>
2023-01-01 22:06:04 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+//config to read env file
+dotenv.config();
+
+app.use(bodyParser.json({limit:"50mb"}));
+app.use(morgan("common"));
+app.use(cors())
+
+// connect db
+mongoose.connect(process.env.MONGODB_URL, () => {
+    console.log("Connected to MongoDB")
+})
+app.get("/api", (req, res)=> {
+    res.status(200).json("Hello")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running...");
+})
